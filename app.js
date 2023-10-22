9e43c357a6b2d0ee757184bbdbdace267cf7e6a6
jetzerszeri <jetzers.2@gmail.com>
2023-10-22 19:05:33 -0400

    feat: install cors to add fata from html

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const comentariosRouter  = require('./routes/comentarios');
 require('dotenv').config();
 const app = express();
 const port = 2024;
+const cors = require('cors');
+app.use(cors());
 
 app.use( express.json() );
 
